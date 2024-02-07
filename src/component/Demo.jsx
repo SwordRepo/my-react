90685b3cadc92bd30ffb1882c189c2e14091bf33
maha <mahakumar197@gmail.com>
2024-02-07 15:33:26 +0530

    demo card fix

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -19,113 +19,136 @@ function DemoTwo() {
   return (
     <div>
       <div className="container-fluid review-container">
-        <img src={woman} alt="" className="image-women" />
-        <div className="bd-example container">
-          <Carousel breakPoints={breakPoints} itemsToShow={1} infiniteLoop>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                </div>
-
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
+        <div className=" container">
+          <div className="row">
+            <div className="col-2">
+              <img src={woman} alt="" className="image-women" />
             </div>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                </div>
+            <div className="col-10">
+              <Carousel
+                itemsToShow={3}
+                renderArrow={({ type, onClick }) => (
+                  <div
+                    onClick={onClick}
+                    className={`custom-${type}-icon`}
+                    style={{
+                      fontSize: "2rem",
+                      marginTop: "13rem",
+                      color: "#005DA5",
+                      fontWeight: "600",
+                    }}
+                  >
+                    {/* Your custom icon or image */}
+                    {type === "PREV" ? "<" : ">"}
+                  </div>
+                )}
+              >
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
 
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
-            </div>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
                 </div>
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
 
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
-            </div>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Maha</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
                 </div>
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
 
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
-            </div>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
                 </div>
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Maha</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
 
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
-            </div>
-            <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
-              <div class="card-body card-review">
-                <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
-                <div className="rating-star">
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
-                  <img src={img} alt="" className="image-rating" />
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
                 </div>
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
 
-                <p class="card-text card-body-text">
-                  I found that the people are very helpful and understanding, I
-                  would definitely recommend them.
-                </p>
-              </div>
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
+                </div>
+                <div class="card col-3 p-0 mt-4 mx-4 card-carosel">
+                  <div class="card-body card-review">
+                    <h5 class="card-title mb-4 fw-bold fs-3">Steve</h5>
+                    <div className="rating-star">
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                      <img src={img} alt="" className="image-rating" />
+                    </div>
+
+                    <p class="card-text card-body-text">
+                      I found that the people are very helpful and
+                      understanding, I would definitely recommend them.
+                    </p>
+                  </div>
+                </div>
+                {/* Add two more divs for the remaining two cards */}
+              </Carousel>
             </div>
-            {/* Add two more divs for the remaining two cards */}
-          </Carousel>
+          </div>
         </div>
       </div>
     </div>
