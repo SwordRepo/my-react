d5d346f31b860dfc46a9c2e5ba3dc54d2d96b01b
mahalakshmi <mahalakshmi.j@swordgroup.in>
2024-02-14 00:52:36 +0530

    nav and banner fix

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import img1 from "./images/image.jpg";
+import img1 from "./images/ban5.webp";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import records from "../records.json";
+
 
 export default function Banner(props) {
   const {
@@ -14,27 +14,55 @@ export default function Banner(props) {
     bannerHeaderline,
   } = props.data;
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6 text-dec">
-          <div className="text-dec">
+    // <div className="container">
+    //   <div className="row">
+    //     <div className="col-12 col-md-6 text-dec">
+    //       <div className="text-dec">
+    //         <h1 className="text_img ">
+    //           {bannerHeader}
+    //           <strong className="bannerHeaderline">{bannerHeaderline}</strong>
+    //         </h1>
+    //       </div>
+
+    //       <p className="fs-2 text-primary">{bannerSubHeader}</p>
+
+    //       <a href="#" className="banner-btn btn btn-primary fw-bold my-4 ">
+    //         {bannerBtn}
+    //       </a>
+    //       <p className="banner-footer-text">{bannerText}</p>
+    //     </div>
+    //     <div className="col-12 col-md-6">
+    //       <img src={img1} className="img-banner" alt="Logo" />
+    //     </div>
+    //   </div>
+    // </div>
+     <div className="container-fluid  p-0">
+      {/* Image taking full width */}
+      <img
+        src={img1} className="img-banner"
+        alt="Logo"
+        
+       
+      />
+
+     <div className="container">
+<div className="hero-banner">
+       <div className="text-dec">
             <h1 className="text_img ">
-              {bannerHeader}
-              <strong className="bannerHeaderline">{bannerHeaderline}</strong>
+               {bannerHeader}
+               <strong className="bannerHeaderline">{bannerHeaderline}</strong>
             </h1>
           </div>
 
-          <p className="fs-2 text-primary">{bannerSubHeader}</p>
+           <p className="fs-2 text-primary">{bannerSubHeader}</p>
 
-          <a href="#" className="banner-btn btn btn-primary fw-bold my-4 ">
-            {bannerBtn}
+           <a href="#" className="banner-btn btn btn-primary fw-bold my-4 ">
+             {bannerBtn}
           </a>
-          <p className="banner-footer-text">{bannerText}</p>
-        </div>
-        <div className="col-12 col-md-6">
-          <img src={img1} className="img-banner" alt="Logo" />
-        </div>
+           <p className="banner-footer-text">{bannerText}</p>
       </div>
+     </div>
+      
     </div>
   );
 }
--- src/component/NavBar/Nav.jsx
+++ src/component/NavBar/Nav.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import "./Navbar.css"; // Make sure to import your CSS file
+import "./Navbar.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS if needed
+import imgs from "./images/logo.png"
 
 export default function Nav(props) {
-  const { logo, menus, btn } = props.data;
+  const {  menus, btn } = props.data;
   const [activeMenu, setActiveMenu] = useState(null);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const handleMenuClick = (menu) => {
+  const handleMenuHover = (menu) => {
     setActiveMenu(menu);
     setMobileMenuOpen(false);
   };
@@ -18,13 +19,15 @@ export default function Nav(props) {
 
   return (
     <nav
-      className={`container navbar bg-white ${
+      className={`container navbar bg-white  ${
         mobileMenuOpen ? "mobile-open" : ""
       }`}
     >
-      {/* Logo */}
-      <div className="navbar-header">
-        <img src={logo} alt="Logo" />
+      <div className="d-flex align-items-center">
+         
+      
+      <div className="navbar-header ">
+        <img src={imgs} className="nav-logo"  />
         <button
           className="mobile-menu-btn d-sm-block d-md-none d-lg-none"
           onClick={handleMobileMenuToggle}
@@ -33,46 +36,36 @@ export default function Nav(props) {
         </button>
       </div>
 
-      {/* Main Menu */}
-      <ul className={`menu-list ${mobileMenuOpen ? "mobile-open" : ""}`}>
+     
+      <ul className={` menu-list ${mobileMenuOpen ? "mobile-open" : ""}`}>
         {menus.map((menu) => (
           <li
             key={menu.menu}
             className={`menu-item ${activeMenu === menu.menu ? "active" : ""}`}
-            onClick={() => handleMenuClick(menu.menu)}
+            onMouseEnter={() => handleMenuHover(menu.menu)} // Changed onClick to onMouseEnter
+            onMouseLeave={() => setActiveMenu(null)} // Hide submenu on mouse leave
           >
             {menu.menu}
+            {/* Render submenu if the current menu is active */}
+            {activeMenu === menu.menu && (
+              <ul className="sub-menu">
+                {menu.subMenus.map((subMenu) => (
+                  <li className="sub-menu-item" key={subMenu.submenu}>
+                    {subMenu.submenu}
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
 
-      {/* Sub Menu */}
-      {activeMenu && (
-        <ul
-          className={`menu-list sub-menu ${
-            mobileMenuOpen ? "mobile-open" : ""
-          }`}
-        >
-          {menus
-            .filter((menu) => menu.menu === activeMenu)
-            .map((menu) => (
-              <li key={menu.menu}>
-                <ul className="menu-list">
-                  {menu.subMenus.map((subMenu) => (
-                    <li className="menu-item" key={subMenu.submenu}>
-                      {subMenu.submenu}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-        </ul>
-      )}
-
-      {/* Call-to-Action Button */}
-      <a href="#" className="btn btn-primary fw-bold my-4 btn-nav">
+      <div className="">
+<a href="#" className="btn btn-primary fw-bold my-4 btn-nav">
         {btn}
       </a>
+      </div>
+      </div>
     </nav>
   );
 }
